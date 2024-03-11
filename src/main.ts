8921572a463ddc35f88ca808a028dddd0e21581f
Egor <itpd2010@yandex.ru>
2024-03-11 10:35:36 +0100

    feat: PORT value is stored in .env file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+  dotenv.config();
+
+  const port = process.env.PORT || 4000;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  await app.listen(port);
 }
 bootstrap();
