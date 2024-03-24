e1f7b0d9827fb4a282634b3fcca2a47c5e5291d5
Egor <itpd2010@yandex.ru>
2024-03-24 20:06:15 +0100

    feat: docker + typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,35 @@ import { ArtistsModule } from './artists/artists.module';
 import { AlbumModule } from './album/album.module';
 import { FavsModule } from './favs/favs.module';
 import { DbModule } from './db/db.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/entities/user.entity';
+import { Track } from './tracks/entities/tracks.entity';
+import { Artist } from './artists/entities/artists.entity';
+import { Album } from './album/entities/album.entity';
+import { Fav } from './favs/entities/fav.entity';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule, TracksModule, ArtistsModule, AlbumModule, FavsModule, DbModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env'
+    }),
+    UsersModule,
+    TracksModule,
+    ArtistsModule,
+    AlbumModule,
+    FavsModule,
+    DbModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [User, Track, Artist, Album, Fav],
+      synchronize: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,8 +1,22 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class User {
+  @PrimaryGeneratedColumn()
   id: string; // uuid v4
+
+  @Column()
   login: string;
+
+  @Column()
   password: string;
+
+  @Column()
   version: number; // integer number, increments on update
+
+  @Column()
   createdAt: number; // timestamp of creation
+
+  @Column()
   updatedAt: number; // timestamp of last update
 }
